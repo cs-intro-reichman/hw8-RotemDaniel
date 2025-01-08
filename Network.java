937ad2576b6f03c1605d91c53340ea192b039839
rotem <rotemd884@gmail.com>
2025-01-08 12:33:35 +0200

    test4

--- Network.java
+++ Network.java
@@ -138,9 +138,9 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they
     // follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network:";
         for (int i = 0; i < userCount; i++) {
-            ans += users[i] + "\n";
+            ans += "\n" + users[i];
         }
         return ans;
     }
